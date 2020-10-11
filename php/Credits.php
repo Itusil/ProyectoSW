be14bcf3d6b088a0f782fad4f95e45b688170136
unknown <mikeliturria1@gmail.com>
2020-10-11 11:57:39 +0200

    first commit

---
+++ php/Credits.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php include '../html/Head.html'?> 
+<link rel="stylesheet" href="../styles/Style.css">
+</head> 
+<body> 
+<?php include '../php/Menus.php' ?>
+<section class="main" id="s1"> 
+<div>
+	<h2>DATOS DEL AUTOR/AUTORES</h2>
+</div> 
+</section>
+<section>
+<div>
+	<br><br>
+	<p>Mikel Iturria
+	<br>
+	Especialidad en Ingenieria del Software
+	<br><br>
+	<img src="../images/perfil.jpg" height="150">
+	<br><br>
+	<a href="mailto:miturria003@ikasle.ehu.eus">miturria003@ikasle.ehu.eus</a>
+</div> <?php include '../html/Footer.html' ?>
+</section>
+</body> </html>
